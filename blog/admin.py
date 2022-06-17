d40487aac4c23f292a570eb6c1f56f88b7814e64
rdmshine <rdmshine@gmail.com>
2022-06-17 16:15:50 +0900

    admin set up

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Article, Category, Tag
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "author",
+        "title",
+        "article",
+        "is_public",
+        "created_at",
+        "updated_at",
+    )
+    list_display_links = ("pk", "author", "title", "article")
+    list_filter = ("is_public",)
+    search_fields = ("author", "title", "article")
+    filter_horizontal = ("tag_set",)
+    fieldsets = (
+        (
+            "Article",
+            {
+                "fields": (
+                    "author",
+                    "title",
+                    "article",
+                    "category_set",
+                    "tag_set",
+                )
+            },
+        ),
+        (
+            "Disclosure status",
+            {
+                "fields": (
+                    "is_public",
+                )
+            },
+        ),
+    )
+
+
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ["id", "name", "description"]
+#     list_display_links = ["id", "name", "description"]
+#     search_fields = ["id", "name", "description"]
+#
+#
+# @admin.register(Tag)
+# class TagAdmin(admin.ModelAdmin):
+#     list_display = ["id", "name"]
+#     list_display_links = ["id", "name"]
+#     search_fields = ["id", "name"]
